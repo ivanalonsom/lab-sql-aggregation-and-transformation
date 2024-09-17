fc0dd09203935b0d50d6b1abd774d89d71580b5f
ivanalonsom <ivan7yt@gmail.com>
2024-09-17 20:45:34 +0200

    solution lab aggregation and transformation

---
+++ challenge1.sql
@@ -0,0 +1,68 @@
+--1 You need to use SQL built-in functions to gain insights relating to the duration of movies:
+-- 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
+SELECT MAX(length) AS 'max_duration', MIN(length) AS 'min_duration'
+FROM film
+
+
+-- 1.2. Express the average movie duration in hours and minutes. Don't use decimals. Hint: Look for floor and round functions.
+
+SELECT CONCAT( floor(AVG(length)/60), ':', round(AVG(length)%60))
+FROM film
+
+-- 2 You need to gain insights related to rental dates:
+-- 2.1 Calculate the number of days that the company has been operating. Hint: To do this, use the rental table, and the DATEDIFF() function to subtract the earliest date in the rental_date column from the latest date.
+
+SELECT datediff(MAX(rental_date), MIN(rental_date)) AS 'date_diff'
+FROM rental;
+
+-- 2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results.
+
+SELECT *, DATE_FORMAT(rental_date, '%m') AS 'month', DATE_FORMAT(rental_date, '%d') AS 'weekday'
+FROM rental
+LIMIT 20;
+
+-- 2.3 Bonus: Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 'workday', depending on the day of the week. Hint: use a conditional expression.
+
+SELECT *, CASE
+	WHEN DAYNAME(rental_date) LIKE 'Saturday' OR DAYNAME(rental_date) LIKE 'Sunday' THEN 'Weekend'
+	ELSE 'Workday'
+END AS 'DAY_TYPE'
+FROM rental
+LIMIT 20;
+
+--3 Retrieve the film titles and their rental duration. If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
+
+SELECT title, IFNULL(rental_duration, 'Not Available') AS 'Rental duration'
+FROM film
+ORDER BY title ASC
+
+--ALT PEOR
+SELECT title, CASE
+	WHEN rental_duration IS NULL THEN 'Not Available'
+	ELSE rental_duration
+END AS 'Rental duration'
+FROM film
+-- ORDER BY title ASC NOT NECESSARY IN THIS CASE
+
+--4 Retrieve the concatenated first and last names of customers, along with the first 3 characters of their email address
+
+SELECT concat(first_name, last_name,SUBSTRING(email, 1, 3))
+FROM customer
+ORDER BY last_name ASC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ challenge2.sql
@@ -0,0 +1,50 @@
+--1 Next, you need to analyze the films in the collection to gain some more insights. Using the film table, determine:
+-- 1.1 The total number of films that have been released.
+SELECT count(film_id)
+FROM film
+WHERE release_year IS NOT NULL
+
+-- 1.2 The number of films for each rating.
+SELECT rating, count(film_id) AS 'number of films'
+FROM film
+GROUP BY rating
+
+-- 1.3 The number of films for each rating, sorting the results in descending order of the number of films. This will help you to better understand the popularity of different film ratings and adjust purchasing decisions accordingly.
+
+SELECT rating, count(film_id) AS 'number of films'
+FROM film
+GROUP BY rating
+ORDER BY count(film_id) DESC
+
+--2 Using the film table, determine:
+-- 2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
+
+SELECT rating, ROUND(AVG(length),2) 'Mean duration'
+FROM film
+GROUP BY rating
+ORDER BY AVG(length) DESC
+
+-- 2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers who prefer longer movies.
+
+SELECT rating, ROUND(AVG(length),2) 'Mean duration'
+FROM film
+GROUP BY rating
+HAVING ROUND(AVG(length),2) > 120
+
+--3. Bonus: determine which last names are not repeated in the table actor.
+
+SELECT distinct(last_name)
+FROM actor
+
+
+
+
+
+
+
+
+
+
+
+
+
